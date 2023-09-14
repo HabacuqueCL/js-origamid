050b208aba61f97fdec52f6f65009059023d0785
HabacuqueCL <habacuque.cavalcante@gmail.com>
2023-09-13 23:52:13 -0300

    adicionado pasta testeJS

---
+++ testeJS/teste.js
@@ -0,0 +1,8 @@
+var texto = "Isso e uma String";
+var idade = "29";
+var minhaIdade = 29;
+var nome = "Habacuque";
+var sobrenome = "Cavalcante";
+var nomeCompleto = `${nome} ${sobrenome}`;
+var frase = "it's time";
+console.log(texto, idade, minhaIdade, nome, sobrenome, nomeCompleto, frase, typeof nome);
