6cc36ca36e2279ea13b49901842c088c34bbcabf
HabacuqueCL <habacuque.cavalcante@gmail.com>
2023-09-25 23:12:40 -0300

    aula 1 boolean e condicionais

--- testeJS/teste.js
+++ testeJS/teste.js
@@ -1,3 +1,8 @@
-var incremento = 5;
-++incremento
-console.log(incremento)
+let valorIgualADez = 10
+
+if (valorIgualADez) {
+  10 + 1
+  console.log(valorIgualADez)
+} else {
+  console.log('nao e igual a 10');
+}
